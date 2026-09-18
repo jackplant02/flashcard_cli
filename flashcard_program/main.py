d930110d21a5f776270726561fd34e69df85faaa
Jack Plant <91396233+jackplant02@users.noreply.github.com>
2026-09-18 18:09:10 -0400

    adding some stuff

--- flashcard_program/main.py
+++ flashcard_program/main.py
@@ -5,23 +5,27 @@ def main():
     init_db()
 
     print("\nWelcome back!\n")
-    print("Pick pick a subject to begin:\n")
     # option to create a new deck
     print("(1) Create New Deck")
+    print("(2) Delete Existing Deck")
 
     # get all existing subjects
     subjects = get_all_subjects()
 
     for i, subject in enumerate(subjects):
-        print(f"({i+2}) {subject}")
+        print(f"({i+3}) {subject}")
 
-    selection = input("\nPlease enter your selection: ").strip()
+    selection = input("\nPlease select an option to begin: ").strip()
 
     if selection == "1":
-        add_subject(input().strip())
+        add_subject(input("Enter a name for your new deck: ").strip())
+
+    elif selection == "2":
+        print("Which deck would you like to ")
+
 
     while True:
-        entry = input("Please choose one of the following options: (a)dd a card, (s)tudy cards, or (q)uit: ").strip().lower()
+        entry = input("\nPlease choose one of the following options: (a)dd a card, (s)tudy cards, or (q)uit: ").strip().lower()
 
         if entry == "q":
             break
